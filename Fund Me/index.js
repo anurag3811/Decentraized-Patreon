0aed5b53b44b5199206c442ef0cf8e2668a7950d
Anurag Singh <90863914+anurag3811@users.noreply.github.com>
2022-10-22 18:42:10 +0530

    Full Stack modified

--- Fund Me/index.js
+++ Fund Me/index.js
@@ -1,19 +1,34 @@
 import {ethers} from "./ethers-5.6.esm.min.js";
 import {abi, contractaddress, deployer} from "./constants.js"
 
-
-
-let currentlevel;
-
-
 const connectbtn = document.getElementById("connectbtn");
 connectbtn.onclick = connect;
+
+const fundButton = document.getElementById("text");
+
+//For admin:
+
+const balancebox = document.getElementById("balancebox");
+
+const getbal = document.getElementById("getbal");
+getbal.onclick = getbal1;
+
+const alphas1 = document.getElementById("alphas");
+const betas1 = document.getElementById("betas");
+const gammas1 = document.getElementById("gammas");
 
 const refresher = document.getElementById("refresher");
 refresher.onclick = refresh;
 
+const withdraw = document.getElementById("withdraw");
+withdraw.onclick = withdrawing;
+
 const checkbtn = document.getElementById("check");
 checkbtn.onclick = check;
+
+// for funders: 
+
+let currentlevel;
 
 const l1 = document.getElementById("l1");
 l1.onclick = fund1;
@@ -24,26 +39,14 @@ l2.onclick = fund2;
 const l3 = document.getElementById("l3");
 l3.onclick = fund3;
 
-
-
-const getbal = document.getElementById("getbal");
-getbal.onclick = getbal1;
-
-const withdraw = document.getElementById("withdraw");
-withdraw.onclick = chor;
-
-const dabba = document.getElementById("dabba");
-
-const alphas1 = document.getElementById("alphas");
-const betas1 = document.getElementById("betas");
-const gammas1 = document.getElementById("gammas");
+//Functions:
 
 async function connect(){
     if(typeof window.ethereum!== 'undefined'){
         console.log(" present ");
         await window.ethereum.request({method:"eth_requestAccounts"})
-        // document.getElementById("connectbtn").innerHTML = `<div style=" padding-left:27px">CONNECTED</div>`
         document.getElementById("connectbtn").innerHTML = "connected"
+        document.getElementById("connectbtn").style.paddingLeft = '50px'
 
       const provider = new ethers.providers.Web3Provider(window.ethereum)
       const signer = provider.getSigner()
@@ -63,164 +66,50 @@ async function connect(){
           render();
         }
     }else{
-        document.getElementById("text").innerText = "Plz get metamask"
+        document.getElementById("text").innerText = "Please get metamask"
     }  
 }
 
+async function getbal1(){
+  if (typeof window.ethereum !== "undefined") {
+    console.log("pehla");
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner()
+    // const contract = new ethers.Contract(contractaddress, abi, signer)
 
+    try {
+      const bal = await provider.getBalance(contractaddress)
+      balancebox.innerHTML = ethers.utils.formatEther(bal)      
+    } catch (error) {
+      console.log(error)
+    }
+  } else {
+    fundButton.innerHTML = "Please install MetaMask"
+  }
+}
 
-async function check(){
+async function refresh() {
+
   if (typeof window.ethereum !== "undefined") {
     console.log("pehla");
     const provider = new ethers.providers.Web3Provider(window.ethereum)
     const signer = provider.getSigner()
     const contract = new ethers.Contract(contractaddress, abi, signer)
-    let address = document.getElementById("mapper").value;
-    let leveladd = await contract.getlevel(address);
-    document.getElementById("output").innerHTML = ` > > > > > Level: ${leveladd}`;
-    document.getElementById("mapper").value = "";
+
+    let alphas = await contract.getalpha();
+    let betas = await contract.getbeta();
+    let gammas = await contract.getgamma();
+  
+    alphas1.innerHTML = alphas;
+    betas1.innerHTML = betas;
+    gammas1.innerHTML = gammas;
+
   }else{
-      document.getElementById("text").innerText = "Plz get metamask"
-  }   
+      document.getElementById("text").innerText = "Please get metamask"
+  }
 }
 
-async function fund1() {
-    // const ethAmount = document.getElementById("ethAmount").value
-    const ethAmount = "0.0001";
-    console.log(`Funding with ${ethAmount}...`)
-    if (typeof window.ethereum !== "undefined") {
-      console.log("pehla");
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
-      const signer = provider.getSigner()
-      const contract = new ethers.Contract(contractaddress, abi, signer)
-      try {
-        const transactionResponse = await contract.fund(0,1,{
-          value: ethers.utils.parseEther(ethAmount),
-        })
-        console.log("pehla");
-        await listenForTransactionMine(transactionResponse, provider);
-        currentlevel = 1;
-        render();       
-      } catch (error) {
-        console.log(error)
-      }
-    } else {
-      fundButton.innerHTML = "Please install MetaMask"
-    }
-  }
-
-
-
-
-  async function fund2() {
-    // const ethAmount = document.getElementById("ethAmount").value
-    console.log(currentlevel);
-    let ethAmount;
-    const level = 2;
-    if(currentlevel==0){
-      ethAmount = "0.00001";
-    }else{
-      ethAmount = "0.0009";
-    }
-    console.log(ethAmount);
-    if (typeof window.ethereum !== "undefined") {
-      console.log("pehla");
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
-      const signer = provider.getSigner()
-      const contract = new ethers.Contract(contractaddress, abi, signer)
-      try {
-        const transactionResponse = await contract.fund(currentlevel,2,{
-          value: ethers.utils.parseEther(ethAmount),
-        })
-        console.log("pehla2");
-        await listenForTransactionMine(transactionResponse, provider);
-        currentlevel = 2;
-        render();
-        
-      } catch (error) {
-        console.log(error)
-      }
-    } else {
-      fundButton.innerHTML = "Please install MetaMask"
-    }
-  }
-
-  async function fund3() {
-    // const ethAmount = document.getElementById("ethAmount").value
-    // const ethAmount = document.getElementById("inputter").value;
-    
-    console.log(currentlevel);
-    let ethAmount;
-    if(currentlevel==0){
-      ethAmount = "0.01";
-    }else if(currentlevel == 1){
-      ethAmount = "0.0099";
-    }else{
-      ethAmount = "0.00009";
-    }
-    console.log(ethAmount);
-    // console.log(`Funding with ${ethAmount}...`)
-    if (typeof window.ethereum !== "undefined") {
-      console.log("pehla");
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
-      const signer = provider.getSigner()
-      const contract = new ethers.Contract(contractaddress, abi, signer)
-      try {
-        const transactionResponse = await contract.fund(currentlevel,3,{value: ethers.utils.parseEther(ethAmount)})
-        console.log("pehla");
-        await listenForTransactionMine(transactionResponse, provider);
-        currentlevel = 3;
-        render();
-      } catch (error) {
-        console.log(error)
-      }
-    } else {
-      fundButton.innerHTML = "Please install MetaMask"
-    }
-  }
-
-  async function getbal1(){
-    if (typeof window.ethereum !== "undefined") {
-      console.log("pehla");
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
-      const signer = provider.getSigner()
-      // const contract = new ethers.Contract(contractaddress, abi, signer)
-
-      try {
-        const bal = await provider.getBalance(contractaddress)
-        dabba.innerHTML = ethers.utils.formatEther(bal)      
-      } catch (error) {
-        console.log(error)
-      }
-
-
-
-    } else {
-      fundButton.innerHTML = "Please install MetaMask"
-    }
-  }
-
-
-
-  function listenForTransactionMine(transactionResponse, provider) {
-    console.log(`Mining ${transactionResponse.hash}`)
-    return new Promise((resolve, reject) => {
-        try {
-            provider.once(transactionResponse.hash, (transactionReceipt) => {
-                console.log(
-                    `Completed with ${transactionReceipt.confirmations} confirmations. `
-                )
-                resolve()
-            })
-        } catch (error) {
-            reject(error)
-        }
-    })
-}
-
-
-
-async function chor() {
+async function withdrawing() {
   // const ethAmount = document.getElementById("ethAmount").value
   // const ethAmount = document.getElementById("inputter").value;
   // console.log(`Funding with ${ethAmount}...`)
@@ -242,74 +131,164 @@ async function chor() {
   }
 }
 
-function render(){
-  //document.getElementById("myBtn").disabled = true;
-  document.getElementById("mylevel").innerHTML = currentlevel;
-
-  if(currentlevel==1){
-    document.getElementById("l1").disabled = true;
-    document.getElementById("l2").disabled = false;
-    document.getElementById("l3").disabled = false;
-    document.getElementById("premiumcontent").innerHTML = `<div style="display: flex; justify-content: center; ">
-       
-    <img src="imgs/${currentlevel}.png" style="height: 200px; width: 200px; border-radius: 20px; margin-right: 20px;" />
-    <img src="imgs/${currentlevel}.png" style="height: 200px; width: 200px; border-radius: 20px; margin-right: 20px;" />
-    <img src="imgs/${currentlevel}.png" style="height: 200px; width: 200px; border-radius: 20px;" />
-    
-</div>`;
-  }else if(currentlevel == 0){
-    document.getElementById("l1").disabled = false;
-    document.getElementById("l2").disabled = false;
-    document.getElementById("l3").disabled = false;
-    document.getElementById("premiumcontent").innerHTML = `Please subscribe for premium content`;
-  }else if(currentlevel == 2 ){
-    document.getElementById("l1").disabled = true;
-    document.getElementById("l2").disabled = true;
-    document.getElementById("l3").disabled = false;
-    document.getElementById("premiumcontent").innerHTML = `<div style="display: flex; justify-content: center; ">
-       
-    <img src="imgs/${currentlevel}.png" style="height: 200px; width: 200px; border-radius: 20px; margin-right: 20px;" />
-    <img src="imgs/${currentlevel}.png" style="height: 200px; width: 200px; border-radius: 20px; margin-right: 20px;" />
-    <img src="imgs/${currentlevel}.png" style="height: 200px; width: 200px; border-radius: 20px;" />
-    
-</div>`;
-  }else if (currentlevel == 3){
-    document.getElementById("l1").disabled = true;
-    document.getElementById("l2").disabled = true;
-    document.getElementById("l3").disabled = true;
-    document.getElementById("premiumcontent").innerHTML = `<div style="display: flex; justify-content: center; ">
-       
-    <img src="imgs/${currentlevel}.png" style="height: 200px; width: 200px; border-radius: 20px; margin-right: 20px;" />
-    <img src="imgs/${currentlevel}.png" style="height: 200px; width: 200px; border-radius: 20px; margin-right: 20px;" />
-    <img src="imgs/${currentlevel}.png" style="height: 200px; width: 200px; border-radius: 20px;" />
-    
-</div>`;
-  }
-
-
-}
-
-
-
-async function refresh() {
-
+async function check(){
   if (typeof window.ethereum !== "undefined") {
     console.log("pehla");
     const provider = new ethers.providers.Web3Provider(window.ethereum)
     const signer = provider.getSigner()
     const contract = new ethers.Contract(contractaddress, abi, signer)
-
-    let alphas = await contract.getalpha();
-    let betas = await contract.getbeta();
-    let gammas = await contract.getgamma();
-  
-    alphas1.innerHTML = alphas;
-    betas1.innerHTML = betas;
-    gammas1.innerHTML = gammas;
-
-
-
+    let address = document.getElementById("mapper").value;
+    let leveladd = await contract.getlevel(address);
+    document.getElementById("output").innerHTML = ` > > > > > Level: ${leveladd}`;
+    document.getElementById("mapper").value = "";
   }else{
-      document.getElementById("text").innerText = "Plz get metamask"
+      document.getElementById("text").innerText = "Please get metamask"
+  }   
+}
+
+function listenForTransactionMine(transactionResponse, provider) {
+  console.log(`Mining ${transactionResponse.hash}`)
+  return new Promise((resolve, reject) => {
+      try {
+          provider.once(transactionResponse.hash, (transactionReceipt) => {
+              console.log(
+                  `Completed with ${transactionReceipt.confirmations} confirmations. `
+              )
+              resolve()
+          })
+      } catch (error) {
+          reject(error)
+      }
+  })
+}
+
+function render(){
+  document.getElementById("mylevel").innerHTML = currentlevel;
+
+  if(currentlevel==1){
+    l1.disabled = true;
+    l2.disabled = false;
+    l3.disabled = false;
+    document.getElementById("premiumcontent").innerHTML = `<div style="display: flex; justify-content: center; ">
+       
+    <img src="imgs/${currentlevel}.png" class="pre" />
+    <img src="imgs/${currentlevel}.png" class="pre" />
+    <img src="imgs/${currentlevel}.png" class="pre" />
+    
+</div>`;
+  }else if(currentlevel == 0){
+    l1.disabled = false;
+    l2.disabled = false;
+    l3.disabled = false;
+    document.getElementById("premiumcontent").innerHTML = `Please subscribe for premium content`;
+  }else if(currentlevel == 2 ){
+    l1.disabled = true;
+    l2.disabled = true;
+    l3.disabled = false;
+    document.getElementById("premiumcontent").innerHTML = `<div style="display: flex; justify-content: center; ">
+       
+    <img src="imgs/${currentlevel}.png" class="pre" />
+    <img src="imgs/${currentlevel}.png" class="pre" />
+    <img src="imgs/${currentlevel}.png" class="pre" />
+    
+</div>`;
+  }else if (currentlevel == 3){
+    l1.disabled = true;
+    l2.disabled = true;
+    l3.disabled = true;
+    document.getElementById("premiumcontent").innerHTML = `<div style="display: flex; justify-content: center; ">
+       
+    <img src="imgs/${currentlevel}.png" class="pre" />
+    <img src="imgs/${currentlevel}.png" class="pre" />
+    <img src="imgs/${currentlevel}.png" class="pre" />
+    
+</div>`;
   }
 }
+
+async function fund1() {
+  const ethAmount = "0.0001";
+  console.log(`Funding with ${ethAmount}...`)
+  if (typeof window.ethereum !== "undefined") {
+    console.log("pehla");
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner()
+    const contract = new ethers.Contract(contractaddress, abi, signer)
+    try {
+      const transactionResponse = await contract.fund(0,1,{
+        value: ethers.utils.parseEther(ethAmount),
+      })
+      console.log("pehla");
+      await listenForTransactionMine(transactionResponse, provider);
+      currentlevel = 1;
+      render();       
+    } catch (error) {
+      console.log(error)
+    }
+  } else {
+    fundButton.innerHTML = "Please install MetaMask"
+  }
+}
+
+async function fund2() {
+  console.log(currentlevel);
+  let ethAmount;
+  const level = 2;
+  if(currentlevel==0){
+    ethAmount = "0.001";
+  }else{
+    ethAmount = "0.0009";
+  }
+  console.log(ethAmount);
+  if (typeof window.ethereum !== "undefined") {
+    console.log("pehla");
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner()
+    const contract = new ethers.Contract(contractaddress, abi, signer)
+    try {
+      const transactionResponse = await contract.fund(currentlevel,2,{
+        value: ethers.utils.parseEther(ethAmount),
+      })
+      console.log("pehla2");
+      await listenForTransactionMine(transactionResponse, provider);
+      currentlevel = 2;
+      render();
+      
+    } catch (error) {
+      console.log(error)
+    }
+  } else {
+    fundButton.innerHTML = "Please install MetaMask"
+  }
+}
+
+async function fund3() {
+  console.log(currentlevel);
+  let ethAmount;
+  if(currentlevel==0){
+    ethAmount = "0.01";
+  }else if(currentlevel == 1){
+    ethAmount = "0.0099";
+  }else{
+    ethAmount = "0.00009";
+  }
+  console.log(ethAmount);
+  if (typeof window.ethereum !== "undefined") {
+    console.log("pehla");
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const signer = provider.getSigner()
+    const contract = new ethers.Contract(contractaddress, abi, signer)
+    try {
+      const transactionResponse = await contract.fund(currentlevel,3,{value: ethers.utils.parseEther(ethAmount)})
+      console.log("pehla");
+      await listenForTransactionMine(transactionResponse, provider);
+      currentlevel = 3;
+      render();
+    } catch (error) {
+      console.log(error)
+    }
+  } else {
+    fundButton.innerHTML = "Please install MetaMask"
+  }
+}
+
